c9fd1586bfecaeb78810951e966d19e2e4df010f
sudhirkrsingh0601 <mr.sudhirkumarsingh8@gmail.com>
2026-04-28 20:16:15 +0530

    Edited Variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,13 +1,13 @@
 variable "region" {
   description = "AWS region for deployment"
   type        = string
-  default     = "ap-south-1"
+  default     = "eu-north-1"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "key_name" {
